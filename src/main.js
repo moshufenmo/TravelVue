89386a6e30cdabbd142a7dbe66dfc00cdcba5882
Tony <jxt0826@126.com>
2018-12-23 18:19:43 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import './assets/style/reset.css' // 解决不同浏览器展示差异问题
+import './assets/style/border.css' // 解决1像素边框问题
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
